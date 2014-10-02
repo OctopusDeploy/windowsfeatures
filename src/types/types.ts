ef27c6ddba63c3a4ea67a76792d2573b18921765
alfhenrik <henrik.a.andersson@outlook.com>
2014-10-02 15:20:44 +1000

    inital commit

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+// LIBS DEFINITION
+/// <reference path="libs/jquery.d.ts"/>
+/// <reference path="libs/angular.d.ts"/>
+/// <reference path="libs/angular-ui-router.d.ts"/>
+/// <reference path="libs/lodash.d.ts"/>
+
+
+// API TYPES
+
+// APP TYPES
+/// <reference path="app/core.ts"/>
