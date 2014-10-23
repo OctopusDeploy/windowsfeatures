ea9cd508d96aac106bb7cd3dfe150c50b8110f54
alfhenrik <henrik.a.andersson@outlook.com>
2014-10-23 22:30:58 +1000

    Forgot to change the source folder

--- deploy.sh
+++ deploy.sh
@@ -136,7 +136,7 @@ if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then
 fi
 
 # 5. KuduSync
-"$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
+"$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/build" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
 exitWithMessageOnError "Kudu Sync to Target failed"
 
 ##################################################################################################################################
